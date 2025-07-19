13589cdae7e84426ac1a93e24fbcebd516e393ff
Abhishekknegi <abhishekknegi@gmail.com>
2025-07-19 15:58:08 +0530

    backend update

--- backend.tf
+++ backend.tf
@@ -3,7 +3,16 @@ terraform {
     bucket         = "abhi01tf-backend-bucket"
     key            = "dev/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "abhi01tf-lock-table"
     encrypt        = true
   }
 }
+
+#terraform {
+#  backend "s3" {
+#    bucket         = "abhi01tf-backend-bucket"
+#    key            = "dev/terraform.tfstate"
+#    region         = "us-east-1"
+#    dynamodb_table = "abhi01tf-lock-table"
+#    encrypt        = true
+#  }
+#}
